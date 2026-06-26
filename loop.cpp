2a08a4b58b3edded0f134d2d7ac2510152286e2f
UJJWAL SUMAN <ujjwalrajan2@gmail.com>
2026-06-26 14:10:32 +0530

    loop basics completed

---
+++ loop.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+//     for(int i = 1;i<10;i++){
+//         cout << "ujjwal"<< endl;
+//     }
+//     for(int i = 5;i>0;i--){
+//         cout << "ujjwal "<< i<< endl;
+//     }
+
+// while loop
+
+int i = 2;
+while(i<=5){
+    cout<<"Striver"<< i<< endl;
+    i++;
+}
+do{
+    cout<< "striver"<< i << endl;
+    i = i+1;
+
+}while(i<=1);
+
+
+
+
+return 0;
+}
