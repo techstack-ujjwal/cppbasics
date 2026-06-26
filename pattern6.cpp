b135667c1486243fc8298c4239eda221f1e93533
UJJWAL SUMAN <ujjwalrajan2@gmail.com>
2026-06-26 18:04:19 +0530

    p6

---
+++ pattern6.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+// int main(){
+//     for(int i=6;i>0;i--){
+//         for(int j = 1;j<i;j++){
+//             cout<<j;
+//         }cout << endl;
+//         }
+// }
+
+
+int main(){
+    for(int i=1;i<=6;i++){
+        for(int j = 1;j<6-i+1;j++){
+            cout<<j;
+        }cout << endl;
+        }
+}
+
+
+
+
+
+
