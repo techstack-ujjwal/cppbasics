6a687cb1b2f4e6dee83bfb9cef5c223ccc811631
UJJWAL SUMAN <ujjwalrajan2@gmail.com>
2026-06-26 13:56:54 +0530

    array and string done

---
+++ arrayandstring.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// 1D ARRAY
+// int main(){
+//     int arr[5];
+//     // ch arr[5]
+//     cin>> arr[0]>>arr[1]>>arr[2]>>arr[3];
+//     //arr[3] = 16;
+    
+
+//     cout<< arr[3];
+// }
+
+
+// 2D ARRAY
+
+
+// int main (){
+//     int arr[3][5];
+//     arr[1][3] = 78;
+//     cout<<arr[1][3]<<endl;
+//     cout<< arr[2][3];
+//     return 0;
+
+// }
+
+
+//  string
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s = "striver";
+    int len = s.size();
+    s[len-1] = 'z';
+    // dont use ""
+    cout <<s[len - 1];
+    return 0;
+}
