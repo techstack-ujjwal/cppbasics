f41b14f6af6207f1b9dde0f55c28939fd3049777
UJJWAL SUMAN <ujjwalrajan2@gmail.com>
2026-06-26 17:56:40 +0530

    p5

---
+++ pattern5.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+// int main(){
+//     for(int i=5;i>0;i--){
+//         for(int j = 0;j<i;j++){
+//             cout<<"*";
+//         }cout << endl;
+//         }
+// }
+
+
+
+// alternate way(striver)
+int main(){
+    for(int i=1;i<=5;i++){
+        for(int j = 0;j<5-i+1;j++){
+            cout<<"*";
+        }cout << endl;
+        }
+}
