3c3be1a490a4f78d23a84b8ad611f2f966eed07f
UJJWAL SUMAN <ujjwalrajan2@gmail.com>
2026-06-27 14:29:54 +0530

    p14

---
+++ pattern14.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   
+   for(int i = 0; i<=4 ; i++){
+   for(char ch = 'A'; ch<='A' + i; ch++){
+    cout << ch<<" ";
+    
+   }cout<<endl;}
+
+    }
