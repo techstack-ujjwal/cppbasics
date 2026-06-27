ec301c7ff434560f2545b5ac152e3502e7ac633b
UJJWAL SUMAN <ujjwalrajan2@gmail.com>
2026-06-27 15:22:32 +0530

    p18

---
+++ pattern18.cpp
@@ -0,0 +1,19 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main (){
+  for(int i = 0; i<5; i++){
+    for(char ch = 'E'-i;ch<='E';ch++){
+        cout<<ch;
+    }cout<< endl;
+  }
+        
+
+
+
+    
+       
+    }
