553452fc29c1f4c45f9648952398378ab21b9539
UJJWAL SUMAN <ujjwalrajan2@gmail.com>
2026-06-27 14:37:34 +0530

    p15

---
+++ pattern15.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   
+   for(int i = 0; i<=4 ; i++){
+   for(char ch = 'A'; ch<='A' + 4 -i; ch++){
+    cout << ch<<" ";
+    
+   }cout<<endl;}
+
+    }
