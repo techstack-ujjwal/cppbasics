2abb30b71d7889de4fd8599c24a642ec439c8e88
UJJWAL SUMAN <ujjwalrajan2@gmail.com>
2026-06-27 13:13:37 +0530

    p10

--- pattern10.cpp
+++ pattern10.cpp
@@ -4,7 +4,7 @@ int main(){
     for(int i =1; i<=2*5-1;i++)
     {
         int stars = i;
-        if(i>5)stars = 2*5-1;
+        if(i>5)stars = 2*5-i;
         for(int j=1;j<=stars;j++){
             cout<<"*";
         }
