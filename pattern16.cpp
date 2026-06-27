1ce8a91f87905474f41b241f79863fbc0bc750ff
UJJWAL SUMAN <ujjwalrajan2@gmail.com>
2026-06-27 14:45:36 +0530

    p16

---
+++ pattern16.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   
+   for(int i = 0; i<=4 ; i++){
+      char ch = 'A'+i;
+   for(int j = 0; j<=i ; j++){
+    cout << ch<<" ";
+    
+    
+   }cout<<endl;}
+
+    }
